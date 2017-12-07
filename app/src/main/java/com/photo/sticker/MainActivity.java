49107947196f163178723af990cb8bc60ea1d854
Pan Bing <panbing@le.com>
2017-12-07 17:57:05 +0800

    init project.

---
+++ app/src/main/java/com/photo/sticker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.photo.sticker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static com.photo.sticker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
